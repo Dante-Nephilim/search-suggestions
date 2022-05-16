1262fd6b6d1583db5d09f9db1315812943915a1b
Dante-Nephilim <sakkara.ps@gmail.com>
2022-05-16 22:27:50 +0530

    add keyboard, hover functionality

--- src/components/AutoSuggest.jsx
+++ src/components/AutoSuggest.jsx
@@ -1,14 +1,35 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { URL } from "../constants/constants";
 
 export default function AutoSuggest() {
   const [suggestions, setSuggestions] = useState();
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedSuggestionIndex, setSelectedSuggestionIndex] = useState(-1);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+  const inputRef = useRef();
+  const autCompleteRef = useRef();
+
+  const handleClickOutside = (e) => {
+    if (autCompleteRef.current.contains(e.target)) {
+      return;
+    }
+    setIsFocused(false);
+  };
 
   useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    inputRef.current.focus();
+    setSelectedSuggestionIndex(-1);
     setSuggestions(undefined);
     setIsError(false);
     if (searchTerm.length > 0) {
@@ -25,6 +46,9 @@ export default function AutoSuggest() {
       .then((res) => {
         setSuggestions(res.data);
         setIsError(false);
+        if (res.data.length > 0) {
+          setSelectedSuggestionIndex(0);
+        }
       })
       .catch((err) => {
         console.error(err);
@@ -35,8 +59,33 @@ export default function AutoSuggest() {
       });
   };
 
+  const handleKeyDown = (e) => {
+    // up arrow
+    if (e.keyCode === 38) {
+      setSelectedSuggestionIndex((prev) =>
+        prev === 0 ? suggestions.length - 1 : prev - 1
+      );
+    }
+
+    // down arrow
+    if (e.keyCode === 40) {
+      setSelectedSuggestionIndex((prev) => (prev + 1) % suggestions.length);
+    }
+
+    // enter
+    if (e.keyCode === 13) {
+      setSearchTerm(suggestions[selectedSuggestionIndex].name);
+    }
+
+    // esc
+    if (e.keyCode === 27) {
+      setIsFocused(false);
+      inputRef.current.blur();
+    }
+  };
+
   return (
-    <div>
+    <div ref={autCompleteRef}>
       <label htmlFor="name">Name</label>
       <input
         name="name"
@@ -46,16 +95,31 @@ export default function AutoSuggest() {
         onChange={(e) => {
           setSearchTerm(e.target.value);
         }}
+        onKeyDown={handleKeyDown}
+        onFocus={() => {
+          setIsFocused(true);
+        }}
+        ref={inputRef}
         autoComplete="off"
       />
       <div>
-        {suggestions &&
+        {isFocused &&
+          suggestions &&
           suggestions.length > 0 &&
-          suggestions.map((suggestion) => (
+          suggestions.map((suggestion, index) => (
             <div
               key={suggestion.id}
+              onMouseEnter={() => {
+                setSelectedSuggestionIndex(index);
+              }}
               onClick={() => {
                 setSearchTerm(suggestion.name);
+              }}
+              style={{
+                backgroundColor:
+                  index === selectedSuggestionIndex
+                    ? "hsl(120, 50%, 80%)"
+                    : "transparent",
               }}
             >
               {suggestion.name}
